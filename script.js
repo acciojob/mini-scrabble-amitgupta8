2270b356476092889a5b641b89af13d1a8aee568
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-06 22:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+function countLetters() {
+        // Get the input text
+        const inputText = document.getElementById('evaluatedText').value;
+        
+        // Count the number of letters
+        const letterCount = inputText.length;
+        
+        // Update the letter count display
+        document.getElementById('letterCount').innerHTML = letterCount;
+      }
